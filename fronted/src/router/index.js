9231005de60b39e1c2954b466a6df271c0fa172b
Yenya000 <anetmiruk@gmail.com>
2026-05-22 15:44:26 +0300

    развернут vue, настроены роутеры

---
+++ fronted/src/router/index.js
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from 'vue-router';
+//Импорт страниц
+import Home from '../views/Home.vue';
+import Profile from '../views/Profile.vue';
+import Admin from '../views/Admin.vue';
+
+const routes = [
+    {path: "/", name: 'Home', component: Home},
+    {path: "/profile", name: 'profile', component: Profile},
+    {path: "/admin", name: 'Admin', component: Admin}
+];
+
+const router = createRouter({
+    history:  createWebHistory(),
+    routes
+});
+
+export default router;
+
---
+++ fronted/src/views/Profile.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>Лк сотрудников</h1>
+    </div>
+</template>
