37b32d025a99554e45c21724509b707ad34f9e60
Yenya000 <anetmiruk@gmail.com>
2026-06-16 00:27:33 +0300

    Update api.js
    фикс подключения к бэкэнду

--- fronted/src/api.js
+++ fronted/src/api.js
@@ -1,13 +1,13 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api',
   headers: {
     'Content-Type': 'application/json'
   }
 })
 
-// Интерцептор из твоего плана: автоматически проверяет токен перед каждым запросом
+// Интерцептор для токена
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token')
   if (token) {
@@ -17,7 +17,5 @@ api.interceptors.request.use((config) => {
 }, (error) => {
   return Promise.reject(error)
 })
-
-
 
 export default api
