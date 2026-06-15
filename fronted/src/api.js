7e4b610db3b71b48a65a562d388c99a4f697e4ee
Yenya000 <anetmiruk@gmail.com>
2026-06-16 00:58:49 +0300

    Update api.js

--- fronted/src/api.js
+++ fronted/src/api.js
@@ -1,13 +1,12 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api',
+  baseURL: 'https://knowledge-base-pbcr.onrender.com/api',
   headers: {
     'Content-Type': 'application/json'
   }
 })
 
-// Интерцептор для токена
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token')
   if (token) {
